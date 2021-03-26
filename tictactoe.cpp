f16d9eb4097cadda92837d70259d9421b1a75213
p1718423 <shamsdine.ziani@etu.univ-lyon1.fr>
2021-03-26 21:55:19 +0100

    alphabeta last version

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,242 +1,258 @@
-#include <iostream>
-#include <stdlib.h>
-#include <math.h> 
+#include<iostream>
 
 using namespace std;
 
-const char EMPTY = '-';
-char grid[3][3] = {{EMPTY, EMPTY, EMPTY},{EMPTY, EMPTY, EMPTY},{EMPTY, EMPTY, EMPTY}};
-char player;
-char computer;
-char winnerPlayer = '-';
-int index_i, index_j;
+int index1; //index of where the minimax algorithm will play
+bool game = true; //used for the main loop
+char grid[3][3] = {{'_','_','_'},{'_','_','_'},{'_','_','_'}}; //empty grid at first
 
-void showGrid() {
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            cout << grid[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-//returns whether the grid is full or not
-bool gridFull(){
-    int count = 0;
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            if(grid[i][j] != EMPTY) count++;
+//returns true if the grid is full, false elseway
+bool gridFull()
+{
+    for(int i=0;i<3;i++){
+        for(int j=0; j<3;j++){
+            if(grid[i][j] != 'X'){
+                if(grid[i][j] != 'O'){
+                    return false;
+                }
+            }
         }
     }
-    return count == 9;
+    return true;
+}
+
+bool isEmpty(char gridItem){
+    return gridItem == '_';
+}
+
+//display the grid
+void showGrid()
+{
+    for(int i=1;i<=9;i++){
+        if((i-1)%3==0) cout << endl;
+        cout << grid[(i-1)/3][(i-1)%3] << " ";
+    }
+    cout << endl << endl;
+}
+
+//get the winner depending on an integer
+int getWinner(char winner){
+    if(winner == 'O') return 1;
+    if(winner == 'X') return 2;
 }
 
 
-//print who's the winner, only called once the game is done
-void checkWinner(char winner){
-    if(winner == player){
-        cout << "You won ! " << endl;
-    }
-    else if (winner == computer){
-        cout << "The computer won !" << endl;
-    }
-    else{
-        cout << "The game is a draw !" << endl;
-    }
-}
-
-
-char winner(string condition){
-    char winner = NULL;
+//returns a number depending on the state of the game 
+int gameEnd(){
+    int winner = 999;
 
     for(int i=0;i<3;i++){
         //horizontal
-        if(grid[i][0] == grid[i][1] && grid[i][0] == grid[i][2] && grid[2][0] != EMPTY){
-            if(condition == "verifMode")
-                checkWinner(grid[i][0]);
-            return grid[i][0];
+        if(grid[i][0] == grid[i][1] && grid[i][0] == grid[i][2] && !(isEmpty(grid[i][0]))){
+            return getWinner(grid[i][0]);
         }
         //vertical
-        if(grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i] && grid[2][0] != EMPTY){
-            if(condition == "verifMode")
-                checkWinner(grid[0][i]);
-            return grid[0][i];
+        if(grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i] && !(isEmpty(grid[0][i]))){
+            return getWinner(grid[0][i]);
         }
     }
 
     //diagonal
-    if(grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[2][0] != EMPTY){
-        if(condition == "verifMode")
-            checkWinner(grid[0][0]);
-        return grid[0][0];
+    if(grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && !(isEmpty(grid[2][2]))){
+        return getWinner(grid[0][0]);
     }
 
-    if(grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2] && grid[2][0] != EMPTY){
-        if(condition == "verifMode")
-            checkWinner(grid[2][0]);
-        return grid[2][0];
+    if(grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2] && !(isEmpty(grid[2][0]))){
+        return getWinner(grid[2][0]);
     }
 
+    //means it is a draw
     if(gridFull()){
-        if(condition == "verifMode")
-            checkWinner('-');
-        return 'draw';
+        return 3;
     }
 
     return winner;
 }
 
-int minimax(bool tag){
-    int minValue = -9999;
-    int maxValue = 9999;
+//the minimax function
+int minimax(bool isMaximizing)
+{
+
+    int maxValue = -9999;
+    int minValue = 9999;
     int i,j,value = 1;
 
-    switch(winner("minimaxMode")){
-
-        case 'O':
-            if('O' == player){
-                return 10;
-            }
-            return -10;
-
-        case 'X':
-            if('X' == player){
-                return 10;
-            }
-            return -10;
-
-        case 'draw':
+    switch(gameEnd()){
+        case 1:
+            return -100;
+        case 2:
+            return 100;
+        case 0:
             return 0;
-
-        //if we go there, winner() returns NULL meaning there are no winners at the moment
         default:
             break;
     }
 
-    int score[3][3] = {{EMPTY, EMPTY, EMPTY},{EMPTY, EMPTY, EMPTY},{EMPTY, EMPTY, EMPTY}};
+    int score[3][3] = {{1,1,1},{1,1,1},{1,1,1}};//1 means empty
 
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(grid[i][j] == EMPTY){
-                if(minValue > maxValue)
-                    if(tag){
-                        grid[i][j] = 'X';
-                        value = minimax(false);
-                    }
-                    else{
-                        grid[i][j] = 'O';
-                        value = minimax(true);
-                    }
-                grid[i][j] = EMPTY;
-                score[i][j] = value;
+    for(i=1;i<=9;i++)
+        {
+                if(isEmpty(grid[(i-1)/3][(i-1)%3]))
+            {
+                if(minValue > maxValue) 
+                {
+                    if(isMaximizing)
+                {
+                    grid[(i-1)/3][(i-1)%3] = 'X';
+                    value = minimax(false);
+                }
+                else
+                {
+                    grid[(i-1)/3][(i-1)%3] = 'O';
+                    value = minimax(true);
+                }
+                grid[(i-1)/3][(i-1)%3] = '_';
+                score[(i-1)/3][(i-1)%3] = value;
+                }
             }
         }
-    }
 
-    if(tag){
+    if(isMaximizing){
         maxValue = -9999;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(score[i][j] > maxValue && score[i][j] != EMPTY){
-                    maxValue = score[i][j];
-                    index_i = i;
-                    index_j = j; 
-                }
+        for(j=1;j<=9;j++){
+            if(score[(j-1)/3][(j-1)%3] > maxValue && score[(j-1)/3][(j-1)%3] != 1)
+            {
+                maxValue = score[(j-1)/3][(j-1)%3];
+                index1 = j;
             }
         }
         return maxValue;
     }
-
     else{
         minValue = 9999;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(score[i][j] < maxValue && score[i][j] != EMPTY){
-                    minValue = score[i][j];
-                    index_i = i;
-                    index_j = j; 
-                }
+        for(j=1;j<=9;j++)
+        {
+            if(score[(j-1)/3][(j-1)%3] < minValue && score[(j-1)/3][(j-1)%3] != 1)
+            {
+                minValue = score[(j-1)/3][(j-1)%3];
+                index1 = j;
             }
         }
         return minValue;
     }
-
 }
 
-bool gameEnd() {
-    //if there is a winner
-    if(winner("verifMode") != NULL){
-        showGrid();
+//called to check whether a move is incorrect or not
+bool moveIncorrect(char move){
+    if(move < 1 || move > 9) {
+        cout << "The move you chose was not in 1-9 range" << endl;
         return true;
     }
     return false;
 }
 
-bool applyMove(int move, char currentPlayer) {
-    if(move < 1 || move > 9) {
-        cout << "The move you chose was not in 1-9 range" << endl;
-        return false;
+//try to make a move and returns a boolean
+bool makeMove(char player, int move){
+    if(isEmpty(grid[(move-1)/3][(move-1)%3])){
+        grid[(move-1)/3][(move-1)%3] = player;
+        return true;
     }
-
-    //non-elegant way to convert (1-9) to (1-3)x(1-3)
-    int i = ceil(move/3);
-    int j = ((move % 3) - 1)%3;
-
-    if(grid[i][j] == EMPTY) grid[i][j] = currentPlayer;
-    else {
-        cout << "The move you chose was already taken" << endl;
-        return false;
-    }
-    return true;
+    cout << "The move is invalid !" << endl;
+    return false;
 }
 
-void choosePlayer(){
+//ask user to play
+void userPlay(){
+    int move;
 
-    cout << "Choose between circle or cross (type O for the circle and X for the cross)" << endl;
-    cin >> player;
+    do{
+        cout<<endl<<"Enter the move:";
+        cin>>move;
+    } while(!makeMove('O', move));
 
-    if(player == 'O'){
-        computer = 'X';
-    }
-    else{
-        computer = 'O';
-    }
-
+    showGrid();
 }
 
-void play() {
+//is called once at the beginning to know whether the computer plays first or not
+void firstMove(){
+   int move,choice;
 
-    //At first, you choose whether you're going to pick 'O' or 'X'.
-    choosePlayer();
+    //check if input is right
+    do {
+        cout << "You: 'O'        Computer: 'X' " << endl;
+        cout << "Press : 1 to play first;  2 for the computer first: " << endl;
+        cin >> choice;
+        if(cin.fail()) { // check to avoid while loops and errors
+            cin.clear();
+            cin.ignore(1);
+            choice = -1;
+        }
+    } while(!(choice == 1 || choice == 2)); // while the input is incorrect
 
-    while(!gameEnd()) {
-        //the player plays
-        int move;
-        do {
+   if(choice == 1)
+   {
+        do{  
             showGrid();
-            cout << "Choose your move (integer 1-9)" << endl;
+            cout << "Enter the move (1-9):" << endl;
             cin >> move;
-            if(cin.fail()) { // check to avoid while loops and errors
-                cin.clear();
-                cin.ignore(1);
-                move = -1;
-            }
-        } while(!applyMove(move, player)); // while the input is incorrect
 
-        //check if the game is done after the player played
-        if(gameEnd()){
-            break;
+        } while(moveIncorrect(move));
+
+        if(isEmpty(grid[(move-1)/3][(move-1)%3])){
+            grid[(move-1)/3][(move-1)%3] = 'O';
+            showGrid();
         }
 
-        //the computer plays
-        minimax(true);
-        grid[index_i][index_j] = computer;
+   }
+}
 
+//Check if the game finished
+void checkGameEnd(){
+    switch(gameEnd()){
+        case 1:
+            game = false;
+            cout << "You won ! " << endl;
+            break;
+        case 2:
+            game = false;
+            cout << "Computer won !" << endl;
+            break;
+        case 3:
+            game = false;
+            cout << "Draw ! " << endl;
+            break;
+        default:
+            break;
     }
 }
 
-int main() {
+void play(){
+    firstMove();
 
+    while(game)
+    {
+        //The computer plays
+        cout << "Computer is playing: " << endl;
+
+        minimax(true);
+        makeMove('X', index1);
+
+        showGrid();
+
+        //Check if the game ended after the computer played
+        checkGameEnd();
+
+        if(!game) break;
+
+        //The user plays
+        userPlay();
+
+        checkGameEnd();
+    }
+}
+
+int main()
+{
     play();
 
     return 0;
